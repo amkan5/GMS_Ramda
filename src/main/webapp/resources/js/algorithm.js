961a74e8337dbf72c26cbcffd2e61ad842d28581
amkan5 <amlydia@naver.com>
2018-09-14 19:05:23 +0900

    commit

--- src/main/webapp/resources/js/algorithm.js
+++ src/main/webapp/resources/js/algorithm.js
@@ -64,9 +64,8 @@ algo.main = { //main = 변수
 			.html('시작값 x, 마지막값 y, 공차 d인 수열의 합을 구하시오.').appendTo($('#ques'));
 			let arr = [{name:'시작값',id:'sta'},
 						{name:'마지막값',id:'end'},
-						{name:'공차', id:'diff'}];
-			let arr2 = {시작값:'sta',마지막값:'end',공차:'diff'};
-			//let arr =['label','시작값','input','sta','text','br'];
+						{name:'공차', id:'dif'}];
+			let arr2 = {시작값:'sta',마지막값:'end',공차:'dif'};
 			/* 구방법
 			 * for(let i in arr){
 				$('<label>').html(arr[i].name).appendTo($('#ques'));
@@ -78,12 +77,19 @@ algo.main = { //main = 변수
 			$('<input>').attr({id:this.id, type:'text'}).appendTo($('#ques'));
 			$('<br/>').appendTo($('#ques'));
 		};*/
-			$.each(arr2, (i,val)=>{
+		/*	$.each(arr2, (i,val)=>{
 				 $('<label>').html(i).appendTo($('#ques'));
 					$('<input>').attr({id:val, type:'text'}).appendTo($('#ques'));
 					$('<br/>').appendTo($('#ques'));
+			 });*/
+		//jason방법 arr1돌리는거
+			$.each(arr, (i,j)=>{
+				 $('<label>').html(j.name).appendTo($('#ques'));
+				 //$.fn.label('시작값','ques'); 여기서 라벨이 컴포넌트임 input,이런거
+					$('<input>').attr({id:j.id, type:'text'}).appendTo($('#ques'));
+					$('<br/>').appendTo($('#ques'));
 			 });
-
+			
 			/*$('<'+'label'+'>').html('시작값').appendTo($('#ques'));
 			$('<'+'input'+'>').attr({id:'sta',type:'text'}).appendTo($('#ques'));
 			$('<br/>').appendTo($('#ques'));
@@ -95,7 +101,7 @@ algo.main = { //main = 변수
 			$('<label>').html('공차').appendTo($('#ques'));
 			$('<input>').attr({id:'diff',type:'text'}).appendTo($('#ques'));
 			$('<br/>').appendTo($('#ques'));*/
-			
+			$('<h6/>').remove();
 			$('#t__r').html(ques);
 			$('<buttion/>').addClass('btn btn-primary')
 			.attr({type:'button'})
@@ -103,26 +109,88 @@ algo.main = { //main = 변수
 			.appendTo($('#ques'))
 			.click(e=>{
 				$('<h6/>').remove();
-				let res = "숫자를 입력하세요"
 				let sta = $('#sta').val()*1; //*1을하면 int로바뀜 
 				let end =  $('#end').val()*1;
 				let dif =  $('#dif').val()*1;
 				let i = sta; //항
 				let sum = 0;
+				/*$('<h6/>').attr({id:'h6'}).empty().text(
+						
+						).appendTo($('#ques'));*/
+				console.log('i : '+i);
 				$('<h6/>').attr({id:'h6'}).empty().text(($.fn.nullChecker( 
 						[ $('#sta').val(),
 							$('#end').val(),
 							 $('#dif').val()]))?
-							 '성공!!'
+							 ''
 				    	     :'완성하세요'
 						).appendTo($('#ques'));
-				/*while(i<=end){ //1+2+3+4+... 1+3+5+7+... 
-					sum += i;
-					i+= dif;
+				if($('#h6').text() === ''){
+					while(i<=end){ //1+2+3+4+... 1+3+5+7+... 
+						sum += i;
+						i+= dif; 
+						console.log(i+'번째 sum : '+sum);
+					}
+					/*alert('i: '+i,);*/
+					$('#h6').text(sum).appendTo($('#ques'));
 				}
-				res = sum;*/
-				//$('<h6/>').attr({id:'h6'}).appendTo($('#ques'));
+				});
 			});
+		$('<li/>')
+		.attr({id : 'siwt'})
+		.addClass('list-group-item')
+		.appendTo($('#side__menu'));
+		$('<a/>')
+		.attr({href : '#'})
+		.html('스위치수열의 합')
+		.appendTo($('#arith'))
+		.click(e=>{
+			$t__r.empty();
+			$('<div/>') //div란 태그를 하나 만들어라
+			.attr({id:'ques'}) //ques라는 아이디가 하나 만들어짐 ㅎ 
+			.appendTo($t__r);
+			$('<h6/>').remove();
+			$('<h4>')
+			.html('시작값 x, 마지막값 y, 공차 d인 수열의 합을 구하시오.').appendTo($('#ques'));
+			let arr = [{name:'시작값',id:'sta'},
+						{name:'마지막값',id:'end'},
+						{name:'공차', id:'dif'}];
+			$.each(arr, (i,j)=>{
+				 $('<label>').html(j.name).appendTo($('#ques'));
+				 //$.fn.label('시작값','ques'); 여기서 라벨이 컴포넌트임 input,이런거
+					$('<input>').attr({id:j.id, type:'text'}).appendTo($('#ques'));
+					$('<br/>').appendTo($('#ques'));
+			 });
+			 $('<h6/>').remove();
+             $('#t__r').html(ques);
+             $('<buttion/>').addClass('btn btn-primary')
+             .attr({type:'button'})
+             .html('결과보기')
+             .appendTo($('#ques'))
+             .click(e=>{
+                  $('<h6/>').remove();
+                  let sta = $('#sta').val()*1; //*1을하면 int로바뀜
+                  let end =  $('#end').val()*1;
+                  let dif =  $('#dif').val()*1;
+                  let i = sta; //항
+                  let sum = 0;
+                  $('<h6/>').attr({id:'h6'}).empty().text(($.fn.nullChecker(
+                             [ $('#sta').val(),
+                                  $('#end').val(),
+                                   $('#dif').val()]))?
+                                   ''
+                            :'완성하세요'
+                             ).appendTo($('#ques'));
+                  if($('#h6').text() === ''){
+                        while(i<=end){ //1+2+3+4+... 1+3+5+7+...
+                        	sum += i*swi;
+    						i+= dif;
+    						swi *= -1;
+                        }
+                        /*alert('i: '+i,);*/
+                       $('#h6').text(sum).appendTo($('#ques'));
+                  }
+                  });
 		});
 		/*	$('#t__l').html('<a id="arith"><h3>등차수열</h3></a>');  함수를 계속 반복하고있음
 			$('#t__l').append('<a id="fibonacci"><h3>피보나치열</h3></a>');
